99475733aa364bb6f9ad85508a363aac9d559d63
fadhil <134047249+FadhilMulinya@users.noreply.github.com>
2024-11-23 15:03:30 +0300

    My real portfolio

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,115 @@
+"use client";
+import { navlinks } from "@/constants/navlinks";
+import { Navlink } from "@/types/navlink";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useState } from "react";
+import { twMerge } from "tailwind-merge";
+import { Heading } from "./Heading";
+import { socials } from "@/constants/socials";
+import { Badge } from "./Badge";
+import { AnimatePresence, motion } from "framer-motion";
+import { IconLayoutSidebarRightCollapse } from "@tabler/icons-react";
+import { isMobile } from "@/lib/utils";
+
+export const Sidebar = () => {
+  const [open, setOpen] = useState(isMobile() ? false : true);
+
+  return (
+    <>
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            initial={{ x: -200 }}
+            animate={{ x: 0 }}
+            transition={{ duration: 0.2, ease: "linear" }}
+            exit={{ x: -200 }}
+            className="px-6 z-[100] py-10 bg-gray-900 w-64 fixed lg:relative h-screen left-0 flex flex-col justify-between"
+          >
+            <div className="flex-1 overflow-auto">
+              <SidebarHeader />
+              <Navigation setOpen={setOpen} />
+            </div>
+            <div onClick={() => isMobile() && setOpen(false)}>
+              <Badge href="/resume" text="Read Resume" />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <button
+        className="fixed lg:hidden bottom-4 right-4 h-8 w-8 border border-gray-700 rounded-full backdrop-blur-sm flex items-center justify-center z-50"
+        onClick={() => setOpen(!open)}
+      >
+        <IconLayoutSidebarRightCollapse className="h-4 w-4 text-gray-300" />
+      </button>
+    </>
+  );
+};
+
+export const Navigation = ({
+  setOpen,
+}: {
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const pathname = usePathname();
+
+  const isActive = (href: string) => pathname === href;
+
+  return (
+    <div className="flex flex-col space-y-1 my-10 relative z-[100]">
+      {navlinks.map((link: Navlink) => (
+        <Link
+          key={link.href}
+          href={link.href}
+          onClick={() => isMobile() && setOpen(false)}
+          className={twMerge(
+            "text-gray-300 hover:text-white transition duration-200 flex items-center space-x-2 py-2 px-2 rounded-md text-sm",
+            isActive(link.href) && "bg-gray-800 text-white"
+          )}
+        >
+          <link.icon
+            className={twMerge(
+              "h-4 w-4 flex-shrink-0",
+              isActive(link.href) && "text-white"
+            )}
+          />
+          <span>{link.label}</span>
+        </Link>
+      ))}
+
+      <Heading as="p" className="text-sm text-gray-400 pt-10 px-2">
+        Socials
+      </Heading>
+      {socials.map((link: Navlink) => (
+        <Link
+          key={link.href}
+          href={link.href}
+          className="text-gray-300 hover:text-white transition duration-200 flex items-center space-x-2 py-2 px-2 rounded-md text-sm"
+        >
+          <link.icon className="h-4 w-4 flex-shrink-0" />
+          <span>{link.label}</span>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+const SidebarHeader = () => {
+  return (
+    <div className="flex items-center space-x-3">
+      <Image
+        src="/images/logos/Fadhil.png"
+        alt="Avatar"
+        height={40}
+        width={40}
+        className="rounded-full"
+      />
+      <div className="flex flex-col">
+        <p className="font-semibold text-white">John Doe</p>
+        <p className="text-sm text-gray-400">Developer</p>
+      </div>
+    </div>
+  );
+};
+
